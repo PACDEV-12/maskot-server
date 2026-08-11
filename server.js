467b4dfd1c69798650310c8c47a7613346a4e302
PACDEV-12 <mahardaya@undiksha.ac.id>
2026-08-11 08:57:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.post("/chat", async (req, res) => {
             generationConfig: {
               responseModalities: ["AUDIO"],
               speechConfig: {
-                voiceConfig: { prebuiltVoiceConfig: { voiceName: "Charon" } },
+                voiceConfig: { prebuiltVoiceConfig: { voiceName: "Maskot Imigrasi" } },
               },
             },
           }),
